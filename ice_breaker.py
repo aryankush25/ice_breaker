c78204dc0dc429e25945a69824625b8909a80056
Aryan Agarwal <aryankush025@gmail.com>
2025-04-30 01:37:48 +0530

    Switches to Ollama for local LLM inference
    Replaces OpenAI with Ollama to enable local LLM inference using the Mistral model.
    
    This change allows the ice_breaker script to run without relying on external APIs, improving privacy and reducing costs.

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
-import os
+# import os
 
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+# from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 information = """
 Narendra Damodardas Modi[a] (born 17 September 1950)[b] is an Indian politician who has served as the prime minister of India since 2014. Modi was the chief minister of Gujarat from 2001 to 2014 and is the member of parliament (MP) for Varanasi. He is a member of the Bharatiya Janata Party (BJP) and of the Rashtriya Swayamsevak Sangh (RSS), a right-wing Hindu nationalist paramilitary volunteer organisation. He is the longest-serving prime minister outside the Indian National Congress.[4]
@@ -30,10 +32,11 @@ if __name__ == "__main__":
         input_variables=input_variables, template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    llm = ChatOllama(model="mistral")
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | StrOutputParser()
 
-    res = chain.invoke(input={"information": information})
+    res = chain.invoke({"information": information})
 
     print(res)
